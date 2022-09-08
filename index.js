d0436c9fdcc59e177ccf1c3f8cfca985120531ee
lisaweilguni <lisaweilguni@gmx.at>
2022-09-08 10:20:57 +0200

    Change variable names and delete 'ask' function

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 import chalk from 'chalk';
-import prompt from 'prompt';
 import randomColor from 'randomcolor';
 
-const userInput1 = process.argv[2];
-const userInput2 = process.argv[3];
+const userInputHue = process.argv[2];
+const userInputLuminosity = process.argv[3];
 
 let color;
 if (process.argv[2] && process.argv[3]) {
   color = randomColor({
-    luminosity: userInput2,
-    hue: userInput1,
-  });
-} else if (process.argv[2] === 'ask') {
-  const userHue = prompt.get('Choose hue:');
-  const userLum = prompt.get('Choose luminosity (dark or light):');
-  color = randomColor({
-    luminosity: userLum,
-    hue: userHue,
+    luminosity: userInputLuminosity,
+    hue: userInputHue,
   });
 } else if (process.argv[2]) {
   color = randomColor({
     luminosity: 'random',
-    hue: userInput1,
+    hue: userInputHue,
   });
 } else {
   color = randomColor({
